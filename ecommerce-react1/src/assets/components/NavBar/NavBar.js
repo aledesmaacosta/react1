9a3ed389eafd00fd15505fa74e142d4f01cfdc85
Agostina <aledesmaacosta@gmail.com>
2024-02-15 21:20:56 -0300

    commit2

---
+++ ecommerce-react1/src/assets/components/NavBar/NavBar.js
@@ -0,0 +1,13 @@
+const NavBar = () => {
+    return (
+        <nav>
+            <h3>Cherry Shop</h3>
+            <div>
+                <button>Cherry</button>
+                <button>Berry</button>
+                <button>Love</button>
+            </div>
+        </nav>
+    )
+}
+export default NavBar
